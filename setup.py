5ab5937512cee0fc37094454a291674eb0686233
Barbaros Yıldırım <barbaros.yildirim@markafoni.com>
2014-08-29 09:30:29 +0300

    setup datas added.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'RomanAlphabet',
+  packages = ['RomanAlphabet'],
+  version = '0.3',
+  description = 'Roman numeral system, integer numbers converter in eachother',
+  author = 'Barbaros Yıldırım',
+  author_email = 'barbarosaliyildirim@gmail.com',
+  url = 'https://github.com/RedXBeard/RomanNumbers',
+  download_url = 'https://github.com/RedXBeard/RomanNumbers/tarball/0.3',
+  keywords = ['roman', 'romanalphabet', 'integer converter', 'integer to roman', 'roman to integer'],
+  classifiers = [],
+)
